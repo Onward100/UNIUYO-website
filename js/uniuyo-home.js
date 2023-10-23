49e880021151124f20b6268e44c15910cc2ed111
Woruka Onward <worukaonward@gmail.com>
2023-10-23 02:31:43 +0100

    commit

---
+++ js/uniuyo-home.js
@@ -0,0 +1,21 @@
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const prevButton = document.querySelector('.slider-control.prev');
+const nextButton = document.querySelector('.slider-control.next');
+
+let currentIndex = 0;
+
+function moveSlider(direction) {
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    } else if (currentIndex >= slides.length) {
+        currentIndex = 0;
+    }
+
+    const translateX = -currentIndex * 100;
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+prevButton.addEventListener('click', () => moveSlider(-1));
+nextButton.addEventListener('click', () => moveSlider(1));
